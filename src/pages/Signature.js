fecc96f6d426822a37245834b2b84f034f0411de
Leonardo Guzi <leoguzi@hotmail.com>
2021-11-19 10:27:32 -0300

    implemented the signature info card

--- src/pages/Signature.js
+++ src/pages/Signature.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from 'react';
 import { colors } from '../globalStyles';
 import { SmallButton, TitleContainer } from '../common/commonStyles';
 import weeklyImage from '../assets/image04.jpg';
-import MonthlyImage from '../assets/image02.jpg';
+import monthlyImage from '../assets/image02.jpg';
+import signatureInfoImage from '../assets/image03.jpg';
 
 export default function Signature() {
   const [user, setUser] = useState({});
   useEffect(() => {
     setUser({ signature: false });
   }, []);
+
   return (
     <>
       <TitleContainer>
@@ -20,28 +22,57 @@ export default function Signature() {
             : 'Você ainda não assinou nenhum plano, que tal começar agora?'}
         </h2>
       </TitleContainer>
-      <SignatureCard>
-        <img src={weeklyImage} alt='Imagem do plano semanal.' />
-        <span>
-          Você recebe um box por semana. Ideal para quem quer exercer a gratidão
-          todos os dias.
-        </span>
-        <SmallButton>Assinar</SmallButton>
-      </SignatureCard>
-      <SignatureCard>
-        <img src={MonthlyImage} alt='Imagem do plano semanal.' />
-        <span>
-          Você recebe um box por mês. <br />
-          <br />
-          Ideal para quem está começando agora.
-        </span>
-        <SmallButton>Assinar</SmallButton>
-      </SignatureCard>
+      {user.signature ? (
+        <Container>
+          <SignatureInfoCard>
+            <img src={signatureInfoImage} alt='Mulher meditando.' />
+            <InfoLabel>
+              Plano: <Info>{user.signature}</Info>
+            </InfoLabel>
+            <InfoLabel>
+              Data da assinatura: <Info>{}</Info>
+            </InfoLabel>
+            <InfoLabel>
+              Proximas entregas: <Info>{}</Info>
+            </InfoLabel>
+            <div>
+              <Product>Chás</Product>
+              <Product>Produtos organicos</Product>
+              <Product>Incensos</Product>
+            </div>
+          </SignatureInfoCard>
+          <SmallButton>Avaliar Entregas</SmallButton>
+        </Container>
+      ) : (
+        <Container>
+          <SignatureOptionCard>
+            <img src={weeklyImage} alt='Mulher meditando com plantas.' />
+            <span>
+              Você recebe um box por semana. Ideal para quem quer exercer a
+              gratidão todos os dias.
+            </span>
+            <SmallButton>Assinar</SmallButton>
+          </SignatureOptionCard>
+          <SignatureOptionCard>
+            <img src={monthlyImage} alt='Mulher meditando no quarto.' />
+            <span>
+              Você recebe um box por mês. <br />
+              <br />
+              Ideal para quem está começando agora.
+            </span>
+            <SmallButton>Assinar</SmallButton>
+          </SignatureOptionCard>
+        </Container>
+      )}
     </>
   );
 }
-
-const SignatureCard = styled.div`
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const SignatureOptionCard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -60,6 +91,41 @@ const SignatureCard = styled.div`
     color: ${colors.color0};
     font-weight: bold;
     font-size: 18px;
-    margin-bottom: 20px;
+    margin: 15px 0 20px 0;
   }
+`;
+
+const SignatureInfoCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.color4};
+  padding: 0 20px 0 20px;
+  margin: 10px;
+  border-radius: 10px;
+  margin-bottom: 30px;
+  img {
+    display: block;
+    width: 100%;
+    border-radius: 10px;
+  }
+  div {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 0 5px 0;
+  }
+  span {
+    margin-bottom: 5px;
+  }
+`;
+
+const Product = styled.span`
+  color: ${colors.color8};
+`;
+
+const InfoLabel = styled.span`
+  color: ${colors.color0};
+`;
+const Info = styled.span`
+  color: ${colors.color8};
 `;
